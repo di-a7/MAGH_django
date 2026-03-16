712a9209bb1dd436d2f476acd67a45aeefef2262
diya <shresthadiya59@gmail.com>
2026-03-16 11:00:57 +0545

    model creation

--- django1/models.py
+++ django1/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Todolist(models.Model):
+   title = models.CharField(max_length=100)
+   description = models.TextField()
+   status = models.BooleanField()
+
+# models.py -> migration_file -> database_table
+# create_class  makemigrations     migrate
